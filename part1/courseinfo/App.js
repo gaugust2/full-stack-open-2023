623a21d9567a9e5a95996117fd7885121f60c48b
Gerald Augustinepillai <90709244+gaugust2@users.noreply.github.com>
2023-06-03 18:29:16 -0400

    Fixing mistake
    Did not make folders properly

---
+++ part1/courseinfo/App.js
@@ -0,0 +1,90 @@
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>
+        {props.name}
+
+      </h1>
+    </div>
+  )
+}
+
+const Part = (prop) => {
+  return (
+    <div>
+      <p>
+        {prop.name} {prop.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Content = (prop) => {
+  console.log(prop.parts[2].exercises);
+  return (
+    <div>
+      <Part
+        name = {prop.parts[0].name}
+        exercises = {prop.parts[0].exercises}
+      />
+      <Part
+        name = {prop.parts[1].name}
+        exercises = {prop.parts[1].exercises}
+      />
+      <Part
+        name = {prop.parts[2].name}
+        exercises = {prop.parts[2].exercises}
+      />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  var total = 0;
+  props.parts.forEach(element => {
+    total += element.exercises;
+  });
+  return (
+    <div>
+      <p>
+        Number of exercises {total}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7
+      },
+      {
+        name: "State of a component",
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header name={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    
+    </div>
+  )
+}
+//<Content name={course}/>
+//<Total name={name} age={age} />
+export default App
