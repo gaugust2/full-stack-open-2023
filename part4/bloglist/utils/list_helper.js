c539aeda0827104f4c0f72766618b277dc48ee4f
gaugust2 <gaugust2@uwo.ca>
2023-06-24 02:37:12 -0400

    Exercise 4.7

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -20,12 +20,57 @@ const favouriteBlog = (blogs) => {
     }
 
     return blogs.length === 0 
-    ? 0
+    ? null
     : blogs.reduce(reducer, 0)
+}
+
+const mostBlogs = (blogs) => {
+    if(blogs.length === 0) return null
+
+    let counter = 0
+    const reducer = (max, next, i, array) => {
+        if(array.filter(item => item.author === max.author).length >= array.filter(item => item.author === next.author).length ){
+            counter++
+            return max
+        } else {
+            counter = 1
+            return next
+        }
+    }   
+
+    const highestBlogged = blogs.reduce(reducer, 0)
+
+    const returnBlog = {
+        author: highestBlogged.author,
+        blogs: counter
+    }
+
+    return returnBlog
+}
+
+const mostLikes = (blogs) => {
+    if(blogs.length === 0) return null
+
+    const reducer = (max, item) => {
+        return max.likes > item.likes
+        ? max
+        : item
+    }
+
+    const highestLiked = blogs.reduce(reducer, 0)
+
+    const returnBlog = {
+        author: highestLiked.author,
+        likes: highestLiked.likes
+    }
+
+    return returnBlog
 }
   
   module.exports = {
     dummy,
     totalLikes,
-    favouriteBlog
+    favouriteBlog,
+    mostBlogs,
+    mostLikes
   }
