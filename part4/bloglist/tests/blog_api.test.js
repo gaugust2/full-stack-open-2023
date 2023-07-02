36b9a52eef24a1b96a1a94b49d138dbc224f1f8b
gaugust2 <gaugust2@uwo.ca>
2023-07-01 20:58:56 -0400

    Exercise 4.10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -15,10 +15,36 @@ afterAll(async () => {
     await mongoose.connection.close()
 })
 
-test("unique identifier property is 'id' instead of '_id'", async () => {
-    const response = await api.get("/api/blogs");
+test('unique identifier property is "id" instead of "_id"', async () => {
+    const response = await api.get('/api/blogs');
 
     const ids = response.body.map((blog) => blog.id);
 
     ids.forEach((id) => expect(id).toBeDefined)
 });
+
+test('a valid blog can be added', async () => {
+    const response1 = await api.get('/api/blogs')
+    const initialLength = response1.body.length
+
+    const blog = {
+        title:'title_test',
+        author:'test test2',
+        url:'test.com',
+        likes: 3,
+    }
+
+    await api
+    .post('/api/blogs')
+    .send(blog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+    const response2 = await api.get('/api/blogs')
+    const authors = response2.body.map(r => r.author)
+
+    expect(response2.body).toHaveLength(initialLength + 1)
+    expect(authors).toContain(
+        'test test2'
+      )
+})
