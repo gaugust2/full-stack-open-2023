ac3b9026efd04ab1b1675125a082d04c6198401d
gaugust2 <gaugust2@uwo.ca>
2023-07-01 20:07:37 -0400

    Exercise 4.9

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -5,12 +5,20 @@ const app = require('../app')
 const api = supertest(app)
 
 test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
 }, 100000)
 
 afterAll(async () => {
-  await mongoose.connection.close()
+    await mongoose.connection.close()
 })
+
+test("unique identifier property is 'id' instead of '_id'", async () => {
+    const response = await api.get("/api/blogs");
+
+    const ids = response.body.map((blog) => blog.id);
+
+    ids.forEach((id) => expect(id).toBeDefined)
+});
